b5921594532d05758ba037bbeca5b548d9c884b8
Pavol Rusnak <stick@gk2.sk>
2012-11-12 01:21:36 +0100

    rework to vertical scrolling :-(

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-var scrollNext = function(e, n) {
-  if (!(e.target instanceof HTMLAnchorElement)) {
-    $('html, body').animate({ scrollLeft: $(n).offset().left-16 }, 1000);
-  }
-};
-
 $(function(){
 
-/*
-  $('html, body').mousewheel(function(event, delta, deltaX, deltaY) {
-    this.scrollLeft -= deltaY * 40;
-    event.preventDefault();
-  });
-*/
-
-  $('#box1').click(function(e){ scrollNext(e, "#box2"); });
-  $('#box2').click(function(e){ scrollNext(e, "#box3"); });
-  $('#box3').click(function(e){ scrollNext(e, "#box4"); });
-  $('#box4').click(function(e){ scrollNext(e, "#box5"); });
-  $('#box5').click(function(e){ scrollNext(e, "#box1"); });
+    $('a').bind('click',function(event){
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 28
+        }, 1000);
+        event.preventDefault();
+    });
 
 });
